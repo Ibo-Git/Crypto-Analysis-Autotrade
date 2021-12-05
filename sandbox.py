471d832e81a5a776129d3b7f21b3a8e59793c54d
CedricSchoeni <cedricschoeni123@gmail.com>
2021-12-05 18:44:29 +0100

    Cleanup+implemented sandbox.trade

--- sandbox.py
+++ sandbox.py
@@ -7,6 +7,10 @@ from datetime import datetime
 TICKER_API_URL = 'https://api.kraken.com/0/public/Ticker'
 FEE_API_URL = 'https://api.kraken.com/0/public/AssetPairs?pair=XXBTZUSD'
 
+API_URLS = {
+    'AssetPairTickerInfo': 'https://api.kraken.com/0/public/Ticker'
+}
+
 # da fees immer fix, braucht man das nich mehr, kannst einmal auslesen und speichern :D
 def get_conversion_fee():
     response = requests.get(FEE_API_URL)
@@ -14,50 +18,84 @@ def get_conversion_fee():
     return response_json['result']['XXBTZUSD']['fees']
 
 class Sandbox:
-    def __init__(self, currencyList):
-        self.currencies = currencyList
+    def __init__(self):
+        self.assets = {}
+        self.transaction_history = []
 
-    def currency_conversion(self, currency_1, currency_2, price = 'a'):
-        pair = {'pair': currency_1 + currency_2}
-        response = requests.get(TICKER_API_URL, pair)
+    def asset_pairs_ticker_info_raw(asset_code_1, asset_code_2):
+        # Todo (2021-12-05), cs: Make sure the pairs are concatenated in the correct order (XXBTUSD is fine USDXXBT not)
+        pair_code = asset_code_1 + asset_code_2
+        data = { 'pair': pair_code }
+        response = requests.get(API_URLS['AssetPairTickerInfo'], data)
         response_json = response.json()
 
-        if not response_json['error']:
-            return float(response_json['result'][currency_1 + currency_2][price][0])
+        if len(response_json['error']) == 0:
+            return response_json['result'][pair_code]
         else:
             raise Exception(response_json['error'][0])
 
-    def convert(self, currency_1, currency_2, amount_currency_1):
-        try:
-            # funktioniert nur in eine Richtung: FIX!!! -> Verkaufen von BTC muss umgerechnet werden!  
-            value = Sandbox.currency_conversion(currency_1, currency_2)
-        except:
-            pass
 
-        try:
-            if amount_currency_1 > self.currencies[currency_1].balance:
-                raise ValueError('You can not sell more than you have bought.')
-            elif amount_currency_1  < 0:
-                raise ValueError('You must sell more than 0$.')
-            
-            # add to history
-            now = datetime.now()
-            transaction_date = now.strftime("%Y/%m/%d - %H:%M:%S")
+    def asset_pairs_ticker_info(asset_code_1, asset_code_2, price = 'a'):
+        raw_info = Sandbox.asset_pairs_ticker_info_raw(asset_code_1, asset_code_2)
 
-        except:
-            pass
+        return (float(raw_info[price][0]))
+
+    
+
+    def trade(self, from_asset_code, to_asset_code, from_asset_amount):
+        asset_conversion_rate = Sandbox.asset_pairs_ticker_info(from_asset_code, to_asset_code)
+
+        # Handle fees
+        fee_percentage = 0.0026
+        from_asset_fee_amount = from_asset_amount * fee_percentage
+        from_asset_amount_after_fee = from_asset_amount - from_asset_fee_amount        
+        to_asset_amount = asset_conversion_rate * from_asset_amount_after_fee
+
+        # Update asset amounts
+        if (from_asset_code not in self.assets): self.assets[from_asset_code] = Asset(from_asset_code)
+        if (to_asset_code not in self.assets): self.assets[to_asset_code] = Asset(to_asset_amount)
+
+        self.assets[from_asset_code].amount -= from_asset_amount
+        self.assets[to_asset_code].amount += to_asset_amount
+
+        # Append to history
+        self.transaction_history.append(TransactionHistoryEvent(
+            transaction_date=datetime.now(),
+            from_asset_code=from_asset_code,
+            to_asset_code=to_asset_code,
+            from_asset_amount=from_asset_amount,
+            to_asset_amount=to_asset_amount,
+            asset_conversion_rate=asset_conversion_rate,
+            from_asset_fee_amount=from_asset_fee_amount,
+            fee_percentage=fee_percentage,            
+            from_asset_amount_after_fee=from_asset_amount_after_fee
+        ))
 
 
-class Currency:
-    def __init__(self, code, balance = 0):
-        self.code = code
-        self.balance = balance
+
+class Asset:
+    def __init__(self, code):
+        self.code  = code
+        self.amount = 0
+
+class TransactionHistoryEvent:
+    def __init__(self, transaction_date, from_asset_code, to_asset_code, from_asset_amount, to_asset_amount, asset_conversion_rate, from_asset_fee_amount, fee_percentage, from_asset_amount_after_fee):
+        self.transaction_date = transaction_date
+        self.from_asset_code = from_asset_code
+        self.to_asset_code = to_asset_code
+        self.from_asset_amount = from_asset_amount
+        self.to_asset_amount = to_asset_amount
+        self.asset_conversion_rate = asset_conversion_rate
+        self.from_asset_fee_amount = from_asset_fee_amount
+        self.fee_percentage = fee_percentage
+        self.from_asset_amount_after_fee = from_asset_amount_after_fee
+        
 
 
 
 def main():
-    test123 = Sandbox()
-    test123.convert('XETH', 'XXBT', 100)
+    sandbox = Sandbox()
+    sandbox.trade('XXBT', 'ZUSD', 100)
 
 
 if __name__ == '__main__':
