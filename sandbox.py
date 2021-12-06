e802c7ce8c2aa59880dac98aa389027f3c118d72
Ibo-Git <ibo1997.it+git@gmail.com>
2021-12-06 16:48:24 +0100

    fixed volume bug

--- sandbox.py
+++ sandbox.py
@@ -44,12 +44,12 @@ class Sandbox:
             raise Exception(response_json['error'][0])
 
 
-    def asset_pairs_ticker_info(self, asset_code_1, asset_code_2, asset_amount):
+    def asset_pairs_ticker_info(self, asset_code_1, asset_code_2, asset_volume):
         
         if asset_code_2 in self.pair_codes[asset_code_1]:
             raw_info = Sandbox.asset_pairs_ticker_info_raw(asset_code_1, asset_code_2)
 
-            if asset_amount >= 0:
+            if asset_volume >= 0:
                 return float(raw_info['a'][0])
             else:
                 return float(raw_info['b'][0])
@@ -57,72 +57,74 @@ class Sandbox:
         elif asset_code_1 in self.pair_codes[asset_code_2]:
             raw_info = Sandbox.asset_pairs_ticker_info_raw(asset_code_2, asset_code_1)
 
-            if asset_amount >= 0:
+            if asset_volume >= 0:
                 return 1 / (float(raw_info['b'][0]))
             else:
                 return 1 / (float(raw_info['a'][0]))
-                
+
         else:
             raise Exception('Pair code not available.')
 
 
-    def trade(self, from_asset_amount, from_asset_code, to_asset_code):
-        asset_conversion_rate = self.asset_pairs_ticker_info(from_asset_code, to_asset_code, from_asset_amount)
+    def trade(self, from_asset_volume, from_asset_code, to_asset_code):
+        asset_conversion_rate = self.asset_pairs_ticker_info(from_asset_code, to_asset_code, from_asset_volume)
 
         # Handle fees
         fee_percentage = 0.0026
         
-        to_asset_amount = asset_conversion_rate * from_asset_amount
-        if from_asset_amount >= 0:
-            to_asset_fee_amount = to_asset_amount * fee_percentage
+        to_asset_volume = asset_conversion_rate * from_asset_volume
+
+        if from_asset_volume >= 0:
+            to_asset_fee_volume = to_asset_volume * fee_percentage
         else:
-            to_asset_fee_amount = - to_asset_amount * fee_percentage
-        to_asset_amount_after_fee = to_asset_amount + to_asset_fee_amount
+            to_asset_fee_volume = - to_asset_volume * fee_percentage
 
-        # Update asset amounts
+        to_asset_volume_after_fee = to_asset_volume + to_asset_fee_volume
+
+        # Update asset volumes
         if (from_asset_code not in self.assets): self.assets[from_asset_code] = Asset(from_asset_code)
-        if (to_asset_code not in self.assets): self.assets[to_asset_code] = Asset(to_asset_amount)
+        if (to_asset_code not in self.assets): self.assets[to_asset_code] = Asset(to_asset_volume)
 
-        self.assets[from_asset_code].amount += from_asset_amount
-        self.assets[to_asset_code].amount -= to_asset_fee_amount
+        self.assets[from_asset_code].volume += from_asset_volume
+        self.assets[to_asset_code].volume -= to_asset_volume_after_fee
 
         # Append to history
         self.transaction_history.append(TransactionHistoryEvent(
             transaction_date=datetime.now(),
             from_asset_code=from_asset_code,
             to_asset_code=to_asset_code,
-            from_asset_amount=from_asset_amount,
-            to_asset_amount=to_asset_amount,
+            from_asset_volume=from_asset_volume,
+            to_asset_volume=to_asset_volume,
             asset_conversion_rate=asset_conversion_rate,
-            from_asset_fee_amount=to_asset_fee_amount,
+            to_asset_fee_volume=to_asset_fee_volume,
             fee_percentage=fee_percentage,            
-            from_asset_amount_after_fee=to_asset_amount_after_fee
+            to_asset_volume_after_fee=to_asset_volume_after_fee
         ))
 
 
 class Asset:
     def __init__(self, code):
         self.code  = code
-        self.amount = 0
+        self.volume = 0
 
 
 class TransactionHistoryEvent:
-    def __init__(self, transaction_date, from_asset_code, to_asset_code, from_asset_amount, to_asset_amount, asset_conversion_rate, from_asset_fee_amount, fee_percentage, from_asset_amount_after_fee):
+    def __init__(self, transaction_date, from_asset_code, to_asset_code, from_asset_volume, to_asset_volume, asset_conversion_rate, to_asset_fee_volume, fee_percentage, to_asset_volume_after_fee):
         self.transaction_date = transaction_date
         self.from_asset_code = from_asset_code
         self.to_asset_code = to_asset_code
-        self.from_asset_amount = from_asset_amount
-        self.to_asset_amount = to_asset_amount
+        self.from_asset_volume = from_asset_volume
+        self.to_asset_volume = to_asset_volume
         self.asset_conversion_rate = asset_conversion_rate
-        self.from_asset_fee_amount = from_asset_fee_amount
+        self.to_asset_fee_volume = to_asset_fee_volume
         self.fee_percentage = fee_percentage
-        self.from_asset_amount_after_fee = from_asset_amount_after_fee
+        self.to_asset_volume_after_fee = to_asset_volume_after_fee
         
 
 def main():
     sandbox = Sandbox()
-    sandbox.trade(10, 'USD', 'XBT')
-    sandbox.trade(-10, 'USD', 'XBT')
+    sandbox.trade(10, 'XBT', 'EUR')
+    sandbox.trade(-433000, 'EUR', 'XBT')
 
 
 if __name__ == '__main__':
