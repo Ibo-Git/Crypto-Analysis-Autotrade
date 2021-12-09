10fe971c5374d7a9c6dcf1373113889f54b73fca
Ibo-Git <ibo1997.it+git@gmail.com>
2021-12-09 22:04:14 +0100

    changed input and target tensors

--- dataProcessing.py
+++ dataProcessing.py
@@ -7,15 +7,18 @@ from transformerModel import TransformerModel, Trainer
 
 
 def main():
+ 
+    decoder_input = torch.tensor([[[0, 0, 0.2, 0.4, 0.6, 0.8], [0, 0, 0.2, 0.4, 0.6, 0.8]], [[0, 0, 0.2, 0.4, 0.6, 0.8], [0, 0, 0.2, 0.4, 0.6, 0.8]]])
+    target_tensor = torch.tensor([[[0, 0.2, 0.4, 0.6, 0.8, 1], [0, 0.2, 0.4, 0.6, 0.8, 1]], [[0, 0.2, 0.4, 0.6, 0.8, 1], [0, 0.2, 0.4, 0.6, 0.8, 1]]])
+
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = TransformerModel(d_model=6, n_heads=3, num_decoder_layers=2).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    model = TransformerModel(d_model=decoder_input.size(dim=2), n_heads=3, num_decoder_layers=2).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.00001)
     criterion = nn.MSELoss()
     trainer = Trainer(model=model, optimizer=optimizer, criterion=criterion, device=device)
     
-    decoder_input = torch.tensor([[[0, 0.2, 0.4, 0.6, 0.8, 1], [0, 0.2, 0.4, 0.6, 0.8, 1]], [[0, 0.2, 0.4, 0.6, 0.8, 1], [0, 0.2, 0.4, 0.6, 0.8, 1]]])
-    target_tensor = torch.tensor([[[0, 0.2, 0.4, 0.6, 0.8, 1], [0, 0.2, 0.4, 0.6, 0.8, 1]], [[0, 0.2, 0.4, 0.6, 0.8, 1], [0, 0.2, 0.4, 0.6, 0.8, 1]]])
-
+    
     for epoch in range(100):
         loss = trainer.train_transformer(decoder_input, target_tensor)
         print(loss)
