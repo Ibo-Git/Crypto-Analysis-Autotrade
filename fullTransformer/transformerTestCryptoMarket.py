dd3c0f9fa10a7ec83cd64923cceedce1a33d0862
CedricSchoeni <cedricschoeni123@gmail.com>
2021-12-12 19:26:15 +0100

    Dataset now configurable from main file

--- fullTransformer/transformerTestCryptoMarket.py
+++ fullTransformer/transformerTestCryptoMarket.py
@@ -10,11 +10,11 @@ from customDataset import CustomDataset
 from transformerModel import Trainer, TransformerModel
 
 
-def data_preprocessing(xbtusd_data, device):
+def data_preprocessing(xbtusd_data, device, encoder_input_length, prediction_length):
     train_sequences = xbtusd_data.head(math.floor(len(xbtusd_data) * 0.8))
     val_sequences = xbtusd_data.tail(math.ceil(len(xbtusd_data) * 0.2))
-    train_ds = CustomDataset(train_sequences)
-    val_ds = CustomDataset(val_sequences)
+    train_ds = CustomDataset(train_sequences, encoder_input_length, prediction_length)
+    val_ds = CustomDataset(val_sequences, encoder_input_length, prediction_length)
     if device.type == 'cpu':
         train_dl = DataLoader(train_ds, batch_size=128, shuffle=True, num_workers=0, pin_memory=True, persistent_workers=False)
         val_dl = DataLoader(val_ds, batch_size=128, shuffle=False, num_workers=0, pin_memory=True, persistent_workers=False)
@@ -27,7 +27,7 @@ def data_preprocessing(xbtusd_data, device):
 
 def main():
     eval_mode = False
-    load_model = True
+    load_model = False
     model_name = 'trained'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
@@ -35,9 +35,9 @@ def main():
     params = {
         # Model
         'feature_size': 4,
-        'n_heads': 4,
-        'num_encoder_layers': 1,
-        'num_decoder_layers': 1,
+        'n_heads': 8,
+        'num_encoder_layers': 4,
+        'num_decoder_layers': 4,
         # Optim
         'optim_name': 'Adam',        
         'optim_lr': 0.0001,
@@ -49,7 +49,11 @@ def main():
     # Data Prep
     xbtusd_data = yf.download(tickers='BTC-USD', period = 'max', interval = '1d')
     xbtusd_data = xbtusd_data / params['asset_scaling']
-    train_dl, val_dl = data_preprocessing(xbtusd_data, device)
+    
+    encoder_input_length = 50
+    prediction_length = 1
+
+    train_dl, val_dl = data_preprocessing(xbtusd_data, device, encoder_input_length, prediction_length)
         
     trainer = None
 
@@ -62,7 +66,9 @@ def main():
             trainer = Trainer.create_trainer(params=checkpoint)
             trainer.load_training(model_name)
         
-        for _ in range(1000):
+        for epoch in range(1000):
+            print(f' --- Epoch: {epoch + 1}')
+
             # Train
             loss = []
             acc = []
