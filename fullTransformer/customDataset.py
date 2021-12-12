b06fc5b9d8329fd4abd4e193942d7f3eff2b3162
Ibo-Git <ibo1997.it+git@gmail.com>
2021-12-12 18:50:23 +0100

    fixed sos and eos tokens

--- fullTransformer/customDataset.py
+++ fullTransformer/customDataset.py
@@ -6,15 +6,18 @@ from torch.utils.data import Dataset
 
 class CustomDataset(Dataset):
     def __init__(self, crypto_data, encoder_input_length=50, prediction_length=3):
+        self.sos_token = torch.zeros(4).unsqueeze(0)
+        self.eos_token = torch.ones(4).unsqueeze(0)
+
         # split entire history into sequences
         sequence_length = encoder_input_length + prediction_length
         sequences = [crypto_data[n:n + sequence_length] for n in range(len(crypto_data) - sequence_length)]
 
+        # create encoder decoder inputs and expected output
         encoder_input = []
         decoder_input = []
         expected_output = []
-        sos_token = [0, 0, 0, 0, 0]
-        eos_token = [1, 1, 1, 1, 1]
+
         for sequence in sequences:
             encoder_input.append(sequence[:encoder_input_length])
             decoder_input.append(sequence[encoder_input_length:sequence_length])
@@ -33,17 +36,17 @@ class CustomDataset(Dataset):
             ])
 
             self.decoder_input.append([
-                torch.cat((torch.tensor(sos_token), torch.tensor(decoder_input[idx]['Open']))),
-                torch.cat((torch.tensor(sos_token), torch.tensor(decoder_input[idx]['High']))),
-                torch.cat((torch.tensor(sos_token), torch.tensor(decoder_input[idx]['Low']))),
-                torch.cat((torch.tensor(sos_token), torch.tensor(decoder_input[idx]['Close'])))
+                torch.tensor(decoder_input[idx]['Open']),
+                torch.tensor(decoder_input[idx]['High']),
+                torch.tensor(decoder_input[idx]['Low']),
+                torch.tensor(decoder_input[idx]['Close'])
             ])
 
             self.expected_output.append([
-                torch.cat((torch.tensor(expected_output[idx]['Open']), torch.tensor(eos_token))),
-                torch.cat((torch.tensor(expected_output[idx]['High']), torch.tensor(eos_token))),
-                torch.cat((torch.tensor(expected_output[idx]['Low']), torch.tensor(eos_token))),
-                torch.cat((torch.tensor(expected_output[idx]['Close']), torch.tensor(eos_token)))
+                torch.tensor(expected_output[idx]['Open']),
+                torch.tensor(expected_output[idx]['High']),
+                torch.tensor(expected_output[idx]['Low']),
+                torch.tensor(expected_output[idx]['Close'])
             ])
 
            
@@ -52,8 +55,8 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, idx):
         encoder_input = torch.stack(self.encoder_input[idx]).permute(1, 0)
-        decoder_input = torch.stack(self.decoder_input[idx]).permute(1, 0)
-        expected_output = torch.stack(self.expected_output[idx]).permute(1, 0)
+        decoder_input = torch.cat((self.sos_token, torch.stack(self.decoder_input[idx]).permute(1, 0)))
+        expected_output = torch.cat((torch.stack(self.expected_output[idx]).permute(1, 0), self.eos_token))
         return encoder_input, decoder_input, expected_output
 
 
